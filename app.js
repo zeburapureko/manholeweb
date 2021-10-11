5e2d35c1c48cadf712181c946c25e08d7ff30faf
yoshihara takao <yoshihara@fando.co.jp>
2021-10-11 08:31:26 +0000

    equip仕様変更前まで

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var equipRouter = require('./routes/equip');
+var boardsRouter = require('./routes/boards');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/equip',equipRouter);
+app.use('/boards',boardsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
